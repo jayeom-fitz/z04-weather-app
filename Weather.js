eafe363b057db5aa574e9ae879d3e68d581ab011
jayeom-fitz <djawjdgus93@naver.com>
2020-12-24 00:04:53 +0900

    #2-2. Icons and Styling

--- Weather.js
+++ Weather.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import PropTypes from "prop-types";
+import { Ionicons, MaterialCommunityIcons  } from "@expo/vector-icons";
 
 export default function Weather({temp, condition}) {
   return (
     <View style={styles.container}>
-      <Text>{temp}</Text>
-      <Text>{condition}</Text>
+      <View style={styles.halfContainer}>
+        <Ionicons name="rainy" size={84} color="black" />
+        <Text style={styles.temp}>
+          {temp}
+          <MaterialCommunityIcons name="temperature-celsius" size={36} color="black" />
+        </Text>
+      </View>
+      <View style={styles.halfContainer}>
+
+      </View>
     </View>
   );
 }
@@ -26,6 +35,16 @@ Weather.propTypes = {
 
 const styles = StyleSheet.create({
   container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center"
+  },
+
+  temp: {
+    fontSize: 36
+  },
+
+  halfContainer: {
     flex: 1,
     justifyContent: "center",
     alignItems: "center"
