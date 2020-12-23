47bf128d56e0bfcd8add45359b8f2708653ced82
jayeom-fitz <djawjdgus93@naver.com>
2020-12-24 00:56:58 +0900

    #2-3. Background Gradient

--- Weather.js
+++ Weather.js
@@ -1,22 +1,108 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, StatusBar } from 'react-native';
 import PropTypes from "prop-types";
-import { Ionicons, MaterialCommunityIcons  } from "@expo/vector-icons";
+import { LinearGradient } from "expo-linear-gradient";
+import { MaterialCommunityIcons  } from "@expo/vector-icons";
+
+const weatherOptions = {
+  Thunderstorm: {
+    iconName: "weather-lightning",
+    gradient: "",
+    mention: ""
+  },
+  Drizzle: {
+    iconName: "weather-pouring",
+    gradient: "",
+    mention: ""
+  },
+  Rain: {
+    iconName: "weather-rainy",
+    gradient: "",
+    mention: ""
+  },
+  Snow: {
+    iconName: "weather-snowy",
+    gradient: "",
+    mention: ""
+  },
+  Mist: {
+    iconName: "",
+    gradient: "",
+    mention: ""
+  },
+  Smoke: {
+    iconName: "",
+    gradient: "",
+    mention: ""
+  },  
+  Haze: {
+    iconName: "weather-hazy",
+    gradient: ["#8e9eab", "#eef2f3"],
+    mention: ""
+  },
+  Dust: {
+    iconName: "",
+    gradient: "",
+    mention: ""
+  },
+  Fog: {
+    iconName: "weather-fog",
+    gradient: "",
+    mention: ""
+  },
+  Sand: {
+    iconName: "",
+    gradient: "",
+    mention: ""
+  },
+  Ash: {
+    iconName: "",
+    gradient: "",
+    mention: ""
+  },
+  Squall: {
+    iconName: "weather-hail",
+    gradient: "",
+    mention: ""
+  },
+  Tornado: {
+    iconName: "weather-tornado",
+    gradient: "",
+    mention: ""
+  },
+  Clear: {
+    iconName: "weather-sunny",
+    gradient: "",
+    mention: ""
+  },
+  Clouds: {
+    iconName: "weather-cloudy",
+    gradient: "",
+    mention: ""
+  }
+};
 
 export default function Weather({temp, condition}) {
   return (
-    <View style={styles.container}>
+    <LinearGradient
+        colors={weatherOptions[condition].gradient}
+        style={styles.container}
+      >
+      <StatusBar barStyle="light-content" />
+      
       <View style={styles.halfContainer}>
-        <Ionicons name="rainy" size={84} color="black" />
+        <MaterialCommunityIcons 
+          name={weatherOptions[condition].iconName} 
+          size={84} color="white" />
         <Text style={styles.temp}>
           {temp}
-          <MaterialCommunityIcons name="temperature-celsius" size={36} color="black" />
+          <MaterialCommunityIcons name="temperature-celsius" size={36} color="white" />
         </Text>
       </View>
       <View style={styles.halfContainer}>
 
       </View>
-    </View>
+    </LinearGradient>
   );
 }
 
@@ -41,7 +127,9 @@ const styles = StyleSheet.create({
   },
 
   temp: {
-    fontSize: 36
+    fontSize: 36,
+    fontWeight: "600",
+    color: 'white'
   },
 
   halfContainer: {
