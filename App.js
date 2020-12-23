b2be653ff3ee991e973779409779d49bb577f276
jayeom-fitz <djawjdgus93@naver.com>
2020-12-23 18:40:22 +0900

    #1-1. Loading Screen

--- App.js
+++ App.js
@@ -1,33 +1,8 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import Loading from "./Loading";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <View style={styles.greenView}>
-        <Text>Hello~!!</Text>
-      </View>
-
-      <View style={styles.redView}>
-        <Text>Hello~!!</Text>
-      </View>
-    </View>
+    <Loading />
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-
-  greenView: {
-    flex: 1,
-    backgroundColor: 'green',
-  },
-
-  redView: {
-    flex: 3,
-    backgroundColor: 'red',
-  },
-});
